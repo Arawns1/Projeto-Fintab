fb918fa3de6fc0c969e55ed078767c86a0f4aea3
Gabriel Damico <gabrieldamico22@gmail.com>
2024-08-25 12:39:34 -0300

    feat(Status): Finalização do endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const resultado = await database.query("SELECT 1 + 1 as sum;");
-  console.log(resultado.rows);
-  response.status(200).json({ chave: "chave" });
+  const updatedAt = new Date().toISOString();
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = dbMaxConnectionsResult.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB;
+  const dbOpenConnectionsResult = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [dbName],
+  });
+  const dbOpenConnectionsValue = dbOpenConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(dbMaxConnectionsValue),
+        opened_connections: dbOpenConnectionsValue,
+        version: dbVersionValue,
+      },
+    },
+  });
 }
 
 export default status;
