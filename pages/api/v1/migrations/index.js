4cefa3ce9278b308fc9b342c9b1f0e4f6f9c604b
Gabriel Damico <gabrieldamico22@gmail.com>
2024-09-01 14:56:36 -0300

    feat: Adição do endpoint `/api/v1/migrations`

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,38 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  try {
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      return response.status(200).json(pendingMigrations);
+    }
+
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+      return response.status(200).json(migratedMigrations);
+    }
+    return response.status(405).end();
+  } catch (error) {
+    return response.status(500).json({ error: error.message });
+  } finally {
+    await dbClient.end();
+  }
+}
